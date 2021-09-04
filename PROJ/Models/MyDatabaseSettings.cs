16754d0889b021ec6e888c092d2676817ec05f20
ClodaghMM <13559048@student.uts.edu.au>
2021-09-04 14:18:01 +1000

    Started mongoDB connection work

---
+++ PROJ/Models/MyDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PROJ.Models
+{
+    public class MyDatabaseSettings
+    {
+        public MyDatabaseSettings()
+        {}
+        public string ConnectionString { get; set; }
+    }
+
+
+
+
+}
--- PROJ/Startup.cs
+++ PROJ/Startup.cs
@@ -24,6 +24,8 @@ namespace PROJ
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MyDataBaseSettings>
+
             services.AddControllersWithViews();
             services.AddSpaStaticFiles(configuration =>
             {
