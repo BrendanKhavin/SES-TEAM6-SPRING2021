715f8303c08e36e67f304beabe96d6e8c83d4d41
Solacx <junyong6@gmail.com>
2021-09-14 13:21:11 +1000

    Use login page as welcome page and update sidebar to access more pages.

--- PROJ/ProjApp/src/app/app-routing.module.ts
+++ PROJ/ProjApp/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'recommendation', component: RecommendationComponent },
   { path: 'login', component: LoginComponent },
