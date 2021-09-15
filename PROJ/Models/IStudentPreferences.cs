d46c9d851f6d4c705f61e8687b964911dd07d549
Adam Kabbara <adamkabbara6@gmail.com>
2021-09-15 19:35:51 +1000

    Implemented preferences class and interface

---
+++ PROJ/Models/IStudentPreferences.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJ.Models
+{
+    public interface IStudentPreferences
+    {
+        List<StudentPreferences> GetAll();
+
+        StudentPreferences GetbyID(int id);
+
+        StudentPreferences Add(StudentPreferences preferences);
+
+        StudentPreferences Delete(int id);
+
+        StudentPreferences updatePreferences(StudentPreferences changedPreferences);
+
+    }
+}
---
+++ PROJ/Models/StudentPreferences.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJ.Models
+{
+    public class StudentPreferences
+    {
+        public int ID { get; set; }
+
+        public int StudentID { get; set; }
+
+        public bool Groupwork { get; set; }
+
+        public bool Essays { get; set; }
+
+        public bool Presentations { get; set; }
+
+        public bool Exams { get; set; }
+
+        public bool Local { get; set; }
+
+    }
+}
