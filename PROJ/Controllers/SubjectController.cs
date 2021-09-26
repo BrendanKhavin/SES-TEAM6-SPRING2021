a8f4902756876e130fbd4178aaef8ae72d62c29e
Andrew Kaskaniotis <akaskaniotis@gmail.com>
2021-09-26 16:31:20 +1000

    SubjectRepository

---
+++ PROJ/Controllers/SubjectController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROJ.Interface;
+using PROJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJ.Controllers
+{
+    public class SubjectController : Controller
+    {
+  
+        public ICompletedSubjectsRepository completedSubjectsRepository { get; set; }
+
+        public SubjectController(ICompletedSubjectsRepository completedSubjectsRepository){
+            this.completedSubjectsRepository = completedSubjectsRepository; 
+        }
+
+        public void AddCompletedSubject(CompletedSubjects completedSubjects){
+            completedSubjectsRepository.InsertCompletedSubject(completedSubjects);
+        }
+
+        public List<CompletedSubjects> GetCompletedSubjects(string userId) {
+            return completedSubjectsRepository.GetCompletedSubjects(userId);
+        }
+
+    }
+}
