75869449abe2babebac7301fbe411b756e5c009d
ClodaghMM <13559048@student.uts.edu.au>
2021-09-29 11:17:12 +1000

    Minor changes

--- PROJ/Controllers/SubjectsController.cs
+++ PROJ/Controllers/SubjectsController.cs
@@ -32,12 +32,6 @@ namespace PROJ.Controllers
         public Subject GetSubjectByCode(string code) =>
             subjectRepository.GetSubjectByCode(code);
 
-        //testing the functionality.
-        // route is api/subjects/{courseArea}
-        [HttpGet("{courseArea:length(10)}")]
-        public List<Subject> GetSubjectsbyCourseArea(string courseArea) =>
-            subjectRepository.GetSubjectsByCourseArea(courseArea);
-
     }
 
 
