561f6f741a52288d745e0c23663ad64100c182d5
Solacx <junyong6@gmail.com>
2021-10-02 17:26:09 +1000

    Fix search and show the appropriate results.

--- PROJ/ProjApp/src/app/search-and-complete-subjects/search-and-complete-subjects.component.ts
+++ PROJ/ProjApp/src/app/search-and-complete-subjects/search-and-complete-subjects.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchAndCompleteSubjectsComponent implements OnInit {
 
+  searchController = new SearchController();
+  searchText = "";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  searchSubjects(): void {
+    this.searchController.search(this.searchText);
+  }
+}
+
+class SearchController {
+  subjects = new Array();
+  results = new Array();
+
+  constructor() {
+    this.subjects.push({ code: "41095", name: "Software Engineering Studio 2A" });
+    this.subjects.push({ code: "41096", name: "Software Engineering Studio 2B" });
+  }
+
+  search(s: string): void {
+    this.results = new Array();
+    this.subjects.forEach(subject => {
+      if (subject.code.includes(s) || subject.name.includes(s)) {
+        this.results.push(subject);
+      }
+    });
+  }
 }
