fb1383536a1068e2065f90598a5fd82d6bee6692
ClodaghMM <13559048@student.uts.edu.au>
2021-10-24 12:38:06 +1100

    Added enrollment model and controller

---
+++ PROJ/Controllers/StudentEnrollmentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PROJ.Models;
+
+namespace PROJ.Controllers
+{
+
+    [Route("api/[controller")]
+    [ApiController]
+    public class StudentEnrollmentController
+    {
+        private IMongoRepository<StudentEnrollment> _studentEnrollmentRepository;
+
+        public StudentEnrollmentController(IMongoRepository<StudentEnrollment> studentEnrollmentRepository)
+        {
+            _studentEnrollmentRepository = studentEnrollmentRepository;
+        }
+
+        //get method
+        [HttpGet("{degree}")]
+        public StudentEnrollment getDegree(string degreeName) =>
+            _studentEnrollmentRepository.FindOne(s => s.degreeName == degreeName);
+
+        
+            
+        
+
+
+   
+
+    }
+}
---
+++ PROJ/Models/StudentEnrollment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PROJ.Models
+
+{
+    public class StudentEnrollment : Document
+    {
+        public int studentId { get; set; }
+        public string degreeName { get; set; }
+        public string major { get; set; }
+
+        public StudentEnrollment()
+        {
+        }
+
+
+        //get student degree
+        //add student degree
+        //update student degree (don't worry about that)
+    }
+}
